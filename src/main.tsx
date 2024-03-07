d8904702a4243e2f5d92507a905aa01d340263a9
devo21 <mboulefacklekaneloic@gmail.com>
2024-03-07 17:50:26 +0100

    ajout des import js dans main.ts 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '../src/assets/js/jquery-3.4.1.min.js'
-import '../src/assets/js/bootstrap.js'
+import './assets/js/jquery-3.4.1.min.js'
+import './assets/js/bootstrap.js'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
