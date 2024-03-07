67f1b18a728ce30d9b190b258442ce390692b51e
devo21 <mboulefacklekaneloic@gmail.com>
2024-03-07 17:54:05 +0100

    ajout des import js dans main.ts 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './assets/js/jquery-3.4.1.min.js'
-import './assets/js/bootstrap.js'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
